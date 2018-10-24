9f6558d4d91127346a9fffc0c2873ff9a080c3f6
Daniel Bennett and Paige Williams <paige@>
2018-10-24 08:26:20 -0700

    Add front end and back end logic

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//Back end
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+  return false;
+  }
+};
+
+
+
+
+
+//Front end
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+        if (!result) {
+          $(".not").text("not");
+        } else {
+          $(".not").text("");
+        }
+
+
+    $("#result").show();
+  });
+});
